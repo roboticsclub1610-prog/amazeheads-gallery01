6ebe80e8dabbad74bee8ba3d78172f9b5ad6e139
roboticsclub1610-prog <roboticsclub1610@gmail.com>
2025-11-30 23:58:56 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const API_BASE = "https://amazeheads-api.roboticsclub1610.workers.dev";
+const PUBLIC_R2_BASE = "https://pub-a8a2250cc9ba7452e4a1437ddc4d1001.r2.dev";
+const DEFAULT_FOLDER = ""; // root inside uploads/
